29b8aaa54bd22af66a9a96d91fe57de1f5a6c196
Gaurav-srivastav44 <gaurav.srivastav_cs23@gla.ac.in>
2026-01-20 15:56:12 +0530

    API creation

--- src/main/java/com/example/day3/controller/StudentController.java
+++ src/main/java/com/example/day3/controller/StudentController.java
@@ -1,12 +1,22 @@
 package com.example.day3.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.day3.model.StudentModel;
+import com.example.day3.service.StudentService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
-    @GetMapping("/")
-    public String student(){
-        return "Student HomePage";
+
+    private StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
     }
+
+    @PostMapping("/add-student")
+    public StudentModel addStudent(@RequestBody StudentModel student) {
+        return service.addStudent(student);
+    }
+
+
 }
